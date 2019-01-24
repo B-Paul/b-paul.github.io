60c2368df18f7b0cfbd07a5040f102e822882038
GuyClicking <41353762+GuyClicking@users.noreply.github.com>
2019-01-24 14:08:26 +1000

    Update index.js

--- 3 style trainer/index.js
+++ 3 style trainer/index.js
@@ -7,3 +7,5 @@ function scrambleCube(algorithm) {
   var solution = cube.solve(); // this will hopefully be different to the algorithm so that the user doesnt have the scramble end up the same as as the solution backwards
   return Cube.inverse(solution);
 }
+
+Document.getElementByID('scramble').innerHTML = scrambleCube("R U R' U'); // test
